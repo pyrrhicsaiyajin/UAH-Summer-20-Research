2fb3c5e94b9a74a01203d744180ff43d67acb482
Andrew Finocchio, Jr <andrew.finocchio.jr@noetic-inc.net>
2020-06-10 01:25:50 -0500

    Pytorch
    installed pytorch

--- testTF2.py
+++ testTF2.py
@@ -8,7 +8,7 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+# import torch.optim as optim
 from torchvision import datasets, models, transforms
 import numpy as np
 import matplotlib.pyplot as plt
